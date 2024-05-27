96cf41f452c9eb03fad9f418088d5c0187ce1911
Jiuberto <111294204+Jiuberto@users.noreply.github.com>
2024-05-27 14:58:49 -0300

    php

---
+++ cabecalho.php
@@ -0,0 +1,14 @@
+<?php
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Flexbox</title>
+	<link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+    <header>
+        <h2><a href="">Menu &equiv;</a></h2>
+        <h1>Lemel</h1>
+    </header>
---
+++ flexteste.php
@@ -0,0 +1,68 @@
+
+<? include 'cabecalho.php'?>
+
+
+    <main>
+        <h2>Login</h2>
+        <h3>Informe seu E-mail</h3>
+        <div>
+            <label for="nome">Email:</label>
+            <input required placeholder="Digite seu email" type="email" name="email" id="email">  <button id="buscar">Buscar</button>
+        </div>
+        <h4>ou</h4>
+        <h3>Entre com</h3>
+        <p class="com">Entra com Facebook</p>
+        <p class="com">Entra com Google</p>
+    </main>
+
+	<footer>
+<div id="exemplo-flaxbox">
+	<div class="caixa">
+		<h2>Contato</h2>
+        <ul>
+            <li>Atentimento</li>
+            <li>Onde encontrar</li>
+            <li>Chat Online</li>
+            <li>SEE ON MAP</li>
+        </ul>
+	</div>
+
+
+
+	<div class="caixa">
+		<h2>Siga</h2>
+        <ul>
+            <li><a href="https://www.instagram.com/lemelmodaeacessorios?igsh=MTlzemhvcGx2cGM3Zw==">@lemelmodaeacessorios</a></li>
+            <li>SEE ON MAP</li>
+	</div>
+
+
+
+	<div class="caixa">
+		<img src="imagens/mini-microsoft.jpg" alt="">
+		<h2>FAQ</h2>
+		<ul>
+            <li>Frete & entrega</li>
+            <li>Garantia</li>
+            <li>Forma de pagamento</li>
+            <li>Troca & devolução</li>
+            <li>Segurança</li>
+            <li>Termos & condições</li>
+        </ul>
+	</div>
+
+    <div class="caixa">
+		<h2>Minha conta</h2>
+        <ul>
+            <li>Meus dados</li>
+            <li>Meus pedidos</li>
+            <li>Minhas trocas</li>
+            <li>Vale compras</li>
+        </ul>
+	</div>
+
+</div>
+</footer>
+
+</body>
+</html>
